e897400c1bfd45e8f77d0f427da786a2f29cffdc
Sumanth Soora <geolight03@gmail.com>
2025-03-09 20:46:41 -0400

    Added the Spring boot intilazer zip file and also set up a mastmusicDb and tried to connect it for the forst time and it ran successfully

---
+++ mastmusic-backend/src/main/java/com/mastmusic/MastmusicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mastmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastmusicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastmusicBackendApplication.class, args);
+	}
+
+}
